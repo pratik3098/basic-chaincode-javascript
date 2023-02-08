c8c2d2b8b1753ab767831ff6753e8e50113ae220
pratik3098 <pratik3098@gmail.com>
2023-02-08 16:59:51 -0500

    customers added

--- test/fareTransfer.test.js
+++ test/fareTransfer.test.js
@@ -13,13 +13,13 @@ const expect = chai.expect;
 const { Context } = require('fabric-contract-api');
 const { ChaincodeStub } = require('fabric-shim');
 
-const AssetTransfer = require('../lib/fareTransfer.js');
+const FareTransfer = require('../lib/fareTransfer.js');
 
 let assert = sinon.assert;
 chai.use(sinonChai);
 
-describe('Asset Transfer Basic Tests', () => {
-    let transactionContext, chaincodeStub, asset;
+describe('Fare Transfer Basic Tests', () => {
+    let transactionContext, chaincodeStub, customer;
     beforeEach(() => {
         transactionContext = new Context();
 
@@ -63,21 +63,20 @@ describe('Asset Transfer Basic Tests', () => {
             return Promise.resolve(internalGetStateByRange());
         });
 
-        asset = {
-            ID: 'asset1',
-            Color: 'blue',
-            Size: 5,
-            Owner: 'Tomoko',
-            AppraisedValue: 300,
+        customer={
+            ID: 'customer1',
+            FirstName: 'Brad',
+            LastName: 'Pitt',
+            TransitId: 'TTC',
         };
     });
 
     describe('Test InitLedger', () => {
         it('should return error on InitLedger', async () => {
             chaincodeStub.putState.rejects('failed inserting key');
-            let assetTransfer = new AssetTransfer();
+            let fareTransfer = new FareTransfer();
             try {
-                await assetTransfer.InitLedger(transactionContext);
+                await fareTransfer.InitLedger(transactionContext);
                 assert.fail('InitLedger should have failed');
             } catch (err) {
                 expect(err.name).to.equal('failed inserting key');
@@ -85,10 +84,10 @@ describe('Asset Transfer Basic Tests', () => {
         });
 
         it('should return success on InitLedger', async () => {
-            let assetTransfer = new AssetTransfer();
-            await assetTransfer.InitLedger(transactionContext);
-            let ret = JSON.parse((await chaincodeStub.getState('asset1')).toString());
-            expect(ret).to.eql(Object.assign({docType: 'asset'}, asset));
+            let fareTransfer = new FareTransfer();
+            await fareTransfer.InitLedger(transactionContext);
+            let ret = JSON.parse((await chaincodeStub.getState('customer1')).toString());
+            expect(ret).to.eql(Object.assign({docType: 'customer'}, customer));
         });
     });
 
@@ -96,9 +95,9 @@ describe('Asset Transfer Basic Tests', () => {
         it('should return error on CreateAsset', async () => {
             chaincodeStub.putState.rejects('failed inserting key');
 
-            let assetTransfer = new AssetTransfer();
+            let fareTransfer = new FareTransfer();
             try {
-                await assetTransfer.CreateAsset(transactionContext, asset.ID, asset.Color, asset.Size, asset.Owner, asset.AppraisedValue);
+                await fareTransfer.CreateAsset(transactionContext, customer.ID, customer.Color, customer.Size, customer.Owner, customer.AppraisedValue);
                 assert.fail('CreateAsset should have failed');
             } catch(err) {
                 expect(err.name).to.equal('failed inserting key');
@@ -106,58 +105,58 @@ describe('Asset Transfer Basic Tests', () => {
         });
 
         it('should return success on CreateAsset', async () => {
-            let assetTransfer = new AssetTransfer();
+            let fareTransfer = new FareTransfer();
 
-            await assetTransfer.CreateAsset(transactionContext, asset.ID, asset.Color, asset.Size, asset.Owner, asset.AppraisedValue);
+            await fareTransfer.CreateAsset(transactionContext, customer.ID, customer.Color, customer.Size, customer.Owner, customer.AppraisedValue);
 
-            let ret = JSON.parse((await chaincodeStub.getState(asset.ID)).toString());
-            expect(ret).to.eql(asset);
+            let ret = JSON.parse((await chaincodeStub.getState(customer.ID)).toString());
+            expect(ret).to.eql(customer);
         });
     });
 
     describe('Test ReadAsset', () => {
         it('should return error on ReadAsset', async () => {
-            let assetTransfer = new AssetTransfer();
-            await assetTransfer.CreateAsset(transactionContext, asset.ID, asset.Color, asset.Size, asset.Owner, asset.AppraisedValue);
+            let fareTransfer = new FareTransfer();
+            await fareTransfer.CreateAsset(transactionContext, customer.ID, customer.Color, customer.Size, customer.Owner, customer.AppraisedValue);
 
             try {
-                await assetTransfer.ReadAsset(transactionContext, 'asset2');
+                await fareTransfer.ReadAsset(transactionContext, 'customer2');
                 assert.fail('ReadAsset should have failed');
             } catch (err) {
-                expect(err.message).to.equal('The asset asset2 does not exist');
+                expect(err.message).to.equal('The customer customer2 does not exist');
             }
         });
 
         it('should return success on ReadAsset', async () => {
-            let assetTransfer = new AssetTransfer();
-            await assetTransfer.CreateAsset(transactionContext, asset.ID, asset.Color, asset.Size, asset.Owner, asset.AppraisedValue);
+            let fareTransfer = new FareTransfer();
+            await fareTransfer.CreateAsset(transactionContext, customer.ID, customer.Color, customer.Size, customer.Owner, customer.AppraisedValue);
 
-            let ret = JSON.parse(await chaincodeStub.getState(asset.ID));
-            expect(ret).to.eql(asset);
+            let ret = JSON.parse(await chaincodeStub.getState(customer.ID));
+            expect(ret).to.eql(customer);
         });
     });
 
     describe('Test UpdateAsset', () => {
         it('should return error on UpdateAsset', async () => {
-            let assetTransfer = new AssetTransfer();
-            await assetTransfer.CreateAsset(transactionContext, asset.ID, asset.Color, asset.Size, asset.Owner, asset.AppraisedValue);
+            let fareTransfer = new FareTransfer();
+            await fareTransfer.CreateAsset(transactionContext, customer.ID, customer.Color, customer.Size, customer.Owner, customer.AppraisedValue);
 
             try {
-                await assetTransfer.UpdateAsset(transactionContext, 'asset2', 'orange', 10, 'Me', 500);
+                await fareTransfer.UpdateAsset(transactionContext, 'customer2', 'orange', 10, 'Me', 500);
                 assert.fail('UpdateAsset should have failed');
             } catch (err) {
-                expect(err.message).to.equal('The asset asset2 does not exist');
+                expect(err.message).to.equal('The customer customer2 does not exist');
             }
         });
 
         it('should return success on UpdateAsset', async () => {
-            let assetTransfer = new AssetTransfer();
-            await assetTransfer.CreateAsset(transactionContext, asset.ID, asset.Color, asset.Size, asset.Owner, asset.AppraisedValue);
+            let fareTransfer = new FareTransfer();
+            await fareTransfer.CreateAsset(transactionContext, customer.ID, customer.Color, customer.Size, customer.Owner, customer.AppraisedValue);
 
-            await assetTransfer.UpdateAsset(transactionContext, 'asset1', 'orange', 10, 'Me', 500);
-            let ret = JSON.parse(await chaincodeStub.getState(asset.ID));
+            await fareTransfer.UpdateAsset(transactionContext, 'customer1', 'orange', 10, 'Me', 500);
+            let ret = JSON.parse(await chaincodeStub.getState(customer.ID));
             let expected = {
-                ID: 'asset1',
+                ID: 'customer1',
                 Color: 'orange',
                 Size: 10,
                 Owner: 'Me',
@@ -169,75 +168,75 @@ describe('Asset Transfer Basic Tests', () => {
 
     describe('Test DeleteAsset', () => {
         it('should return error on DeleteAsset', async () => {
-            let assetTransfer = new AssetTransfer();
-            await assetTransfer.CreateAsset(transactionContext, asset.ID, asset.Color, asset.Size, asset.Owner, asset.AppraisedValue);
+            let fareTransfer = new FareTransfer();
+            await fareTransfer.CreateAsset(transactionContext, customer.ID, customer.Color, customer.Size, customer.Owner, customer.AppraisedValue);
 
             try {
-                await assetTransfer.DeleteAsset(transactionContext, 'asset2');
+                await fareTransfer.DeleteAsset(transactionContext, 'customer2');
                 assert.fail('DeleteAsset should have failed');
             } catch (err) {
-                expect(err.message).to.equal('The asset asset2 does not exist');
+                expect(err.message).to.equal('The customer customer2 does not exist');
             }
         });
 
         it('should return success on DeleteAsset', async () => {
-            let assetTransfer = new AssetTransfer();
-            await assetTransfer.CreateAsset(transactionContext, asset.ID, asset.Color, asset.Size, asset.Owner, asset.AppraisedValue);
+            let fareTransfer = new FareTransfer();
+            await fareTransfer.CreateAsset(transactionContext, customer.ID, customer.Color, customer.Size, customer.Owner, customer.AppraisedValue);
 
-            await assetTransfer.DeleteAsset(transactionContext, asset.ID);
-            let ret = await chaincodeStub.getState(asset.ID);
+            await fareTransfer.DeleteAsset(transactionContext, customer.ID);
+            let ret = await chaincodeStub.getState(customer.ID);
             expect(ret).to.equal(undefined);
         });
     });
 
     describe('Test TransferAsset', () => {
         it('should return error on TransferAsset', async () => {
-            let assetTransfer = new AssetTransfer();
-            await assetTransfer.CreateAsset(transactionContext, asset.ID, asset.Color, asset.Size, asset.Owner, asset.AppraisedValue);
+            let fareTransfer = new FareTransfer();
+            await fareTransfer.CreateAsset(transactionContext, customer.ID, customer.Color, customer.Size, customer.Owner, customer.AppraisedValue);
 
             try {
-                await assetTransfer.TransferAsset(transactionContext, 'asset2', 'Me');
+                await fareTransfer.TransferAsset(transactionContext, 'customer2', 'Me');
                 assert.fail('DeleteAsset should have failed');
             } catch (err) {
-                expect(err.message).to.equal('The asset asset2 does not exist');
+                expect(err.message).to.equal('The customer customer2 does not exist');
             }
         });
 
         it('should return success on TransferAsset', async () => {
-            let assetTransfer = new AssetTransfer();
-            await assetTransfer.CreateAsset(transactionContext, asset.ID, asset.Color, asset.Size, asset.Owner, asset.AppraisedValue);
+            let fareTransfer = new FareTransfer();
+            await fareTransfer.CreateAsset(transactionContext, customer.ID, customer.Color, customer.Size, customer.Owner, customer.AppraisedValue);
 
-            await assetTransfer.TransferAsset(transactionContext, asset.ID, 'Me');
-            let ret = JSON.parse((await chaincodeStub.getState(asset.ID)).toString());
-            expect(ret).to.eql(Object.assign({}, asset, {Owner: 'Me'}));
+            await fareTransfer.TransferAsset(transactionContext, customer.ID, 'Me');
+            let ret = JSON.parse((await chaincodeStub.getState(customer.ID)).toString());
+            expect(ret).to.eql(Object.assign({}, customer, {Owner: 'Me'}));
         });
     });
 
     describe('Test GetAllAssets', () => {
         it('should return success on GetAllAssets', async () => {
-            let assetTransfer = new AssetTransfer();
+            let fareTransfer = new FareTransfer();
 
-            await assetTransfer.CreateAsset(transactionContext, 'asset1', 'blue', 5, 'Robert', 100);
-            await assetTransfer.CreateAsset(transactionContext, 'asset2', 'orange', 10, 'Paul', 200);
-            await assetTransfer.CreateAsset(transactionContext, 'asset3', 'red', 15, 'Troy', 300);
-            await assetTransfer.CreateAsset(transactionContext, 'asset4', 'pink', 20, 'Van', 400);
+            await fareTransfer.CreateAsset(transactionContext, 'customer1', 'blue', 5, 'Robert', 100);
+            await fareTransfer.CreateAsset(transactionContext, 'customer2', 'orange', 10, 'Paul', 200);
+            await fareTransfer.CreateAsset(transactionContext, 'customer3', 'red', 15, 'Troy', 300);
+            await fareTransfer.CreateAsset(transactionContext, 'customer4', 'pink', 20, 'Van', 400);
 
-            let ret = await assetTransfer.GetAllAssets(transactionContext);
+            let ret = await fareTransfer.GetAllAssets(transactionContext);
             ret = JSON.parse(ret);
             expect(ret.length).to.equal(4);
 
             let expected = [
-                {Record: {ID: 'asset1', Color: 'blue', Size: 5, Owner: 'Robert', AppraisedValue: 100}},
-                {Record: {ID: 'asset2', Color: 'orange', Size: 10, Owner: 'Paul', AppraisedValue: 200}},
-                {Record: {ID: 'asset3', Color: 'red', Size: 15, Owner: 'Troy', AppraisedValue: 300}},
-                {Record: {ID: 'asset4', Color: 'pink', Size: 20, Owner: 'Van', AppraisedValue: 400}}
+                {Record: {ID: 'customer1', Color: 'blue', Size: 5, Owner: 'Robert', AppraisedValue: 100}},
+                {Record: {ID: 'customer2', Color: 'orange', Size: 10, Owner: 'Paul', AppraisedValue: 200}},
+                {Record: {ID: 'customer3', Color: 'red', Size: 15, Owner: 'Troy', AppraisedValue: 300}},
+                {Record: {ID: 'customer4', Color: 'pink', Size: 20, Owner: 'Van', AppraisedValue: 400}}
             ];
 
             expect(ret).to.eql(expected);
         });
 
         it('should return success on GetAllAssets for non JSON value', async () => {
-            let assetTransfer = new AssetTransfer();
+            let fareTransfer = new FareTransfer();
 
             chaincodeStub.putState.onFirstCall().callsFake((key, value) => {
                 if (!chaincodeStub.states) {
@@ -246,20 +245,20 @@ describe('Asset Transfer Basic Tests', () => {
                 chaincodeStub.states[key] = 'non-json-value';
             });
 
-            await assetTransfer.CreateAsset(transactionContext, 'asset1', 'blue', 5, 'Robert', 100);
-            await assetTransfer.CreateAsset(transactionContext, 'asset2', 'orange', 10, 'Paul', 200);
-            await assetTransfer.CreateAsset(transactionContext, 'asset3', 'red', 15, 'Troy', 300);
-            await assetTransfer.CreateAsset(transactionContext, 'asset4', 'pink', 20, 'Van', 400);
+            await fareTransfer.CreateAsset(transactionContext, 'customer1', 'blue', 5, 'Robert', 100);
+            await fareTransfer.CreateAsset(transactionContext, 'customer2', 'orange', 10, 'Paul', 200);
+            await fareTransfer.CreateAsset(transactionContext, 'customer3', 'red', 15, 'Troy', 300);
+            await fareTransfer.CreateAsset(transactionContext, 'customer4', 'pink', 20, 'Van', 400);
 
-            let ret = await assetTransfer.GetAllAssets(transactionContext);
+            let ret = await fareTransfer.GetAllAssets(transactionContext);
             ret = JSON.parse(ret);
             expect(ret.length).to.equal(4);
 
             let expected = [
                 {Record: 'non-json-value'},
-                {Record: {ID: 'asset2', Color: 'orange', Size: 10, Owner: 'Paul', AppraisedValue: 200}},
-                {Record: {ID: 'asset3', Color: 'red', Size: 15, Owner: 'Troy', AppraisedValue: 300}},
-                {Record: {ID: 'asset4', Color: 'pink', Size: 20, Owner: 'Van', AppraisedValue: 400}}
+                {Record: {ID: 'customer2', Color: 'orange', Size: 10, Owner: 'Paul', AppraisedValue: 200}},
+                {Record: {ID: 'customer3', Color: 'red', Size: 15, Owner: 'Troy', AppraisedValue: 300}},
+                {Record: {ID: 'customer4', Color: 'pink', Size: 20, Owner: 'Van', AppraisedValue: 400}}
             ];
 
             expect(ret).to.eql(expected);
